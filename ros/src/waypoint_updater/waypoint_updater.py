d8c9310eec0a8679737a629e23fc983005c807ac
Max Power <max.power@springfield.com>
2020-02-08 18:06:03 +0100

    first version of waypoint updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -33,20 +33,55 @@ class WaypointUpdater(object):
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.next_waypoints = None
+        self.map_waypoints = None
+        self.pose = None
 
         rospy.spin()
 
+    def pose_distance(self, index):
+        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2 + (a.z-b.z)**2)
+        return dl(self.map_waypoints[index].pose.pose.position, self.pose.position)
+
+    # find the next waypoint in front of the car
+    def find_next_waypoint(self):
+        min_dist = 10000000000.0
+        next_index = -1
+        if self.map_waypoints is None:
+            rospy.logwarn("No Waypoints given...")
+            return None
+        for i in range(len(self.map_waypoints)):
+            current_distance = self.pose_distance(i)
+            if current_distance < min_dist:
+                min_dist = current_distance
+                next_index = i
+        rospy.loginfo("index of next wp: %s", next_index)
+        return next_index
+
+#
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        self.pose = msg.pose
+        next_index = self.find_next_waypoint()
+        if next_index is None or next_index is -1:
+            return
+        self.next_waypoints = []
+        if next_index >= 0:
+            for i in range(0, LOOKAHEAD_WPS):
+                if next_index >= len(self.map_waypoints):
+                    next_index = 0
+                self.next_waypoints.append(self.map_waypoints[next_index])
+                next_index += 1
+        lane = Lane()
+        lane.waypoints = self.next_waypoints
+        rospy.logdebug("Number of final waypoints: %s", str(len(self.next_waypoints)))
+        self.final_waypoints_pub.publish(lane)
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        rospy.logwarn("Setting waypoints...")
+        self.map_waypoints = waypoints.waypoints
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -62,6 +97,7 @@ class WaypointUpdater(object):
     def set_waypoint_velocity(self, waypoints, waypoint, velocity):
         waypoints[waypoint].twist.twist.linear.x = velocity
 
+    # distance between two waypoints given their indices
     def distance(self, waypoints, wp1, wp2):
         dist = 0
         dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
